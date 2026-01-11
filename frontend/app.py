cd4cdf1f3621027b3efeb8f1d512b562338446a2
haddybhaiya <harshsharma2k614@gmail.com>
2026-01-11 20:26:28 +0530

    changes in api url

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import datetime
 
-API_URL = "https://save-dishes-api.onrender.com/predict"
+API_URL = "https://save-dishes-api.onrender.com"
 
 st.set_page_config(page_title="SaveDishes", layout="centered")
 
